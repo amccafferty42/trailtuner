7e85b797562c1ece2f3eec6203597e7b8e14ccd7
Alex McCafferty <amccafferty42@gmail.com>
2023-08-20 11:50:19 -0400

    add export function

--- export.js
+++ export.js
@@ -1,7 +1,8 @@
-
+let exportedRoute;
+let fullRoute;
 
 function updateGeoJSON() {
-    let exportedRoute = {
+    exportedRoute = {
         features: [trailheadFolder, campsiteFolder],
         "type": "FeatureCollection"
     };
@@ -10,7 +11,7 @@ function updateGeoJSON() {
     for (let day of this.route) exportedRoute.features.push(day.start);
     if(this.route[0].start != this.route[this.route.length - 1].end) exportedRoute.features.push(this.route[this.route.length - 1].end);
 
-    let fullRoute = {
+    fullRoute = {
         "geometry": {
             "type": "LineString",
             "coordinates": []
@@ -46,17 +47,11 @@ function updateGeoJSON() {
                 if (i >= trailFeature.geometry.coordinates.length) i = 0;
             } while ((trailFeature.geometry.coordinates[i][0].toFixed(3) != this.route[this.route.length - 1].end.geometry.coordinates[0].toFixed(3))
                    || (trailFeature.geometry.coordinates[i][1].toFixed(3) != this.route[this.route.length - 1].end.geometry.coordinates[1].toFixed(3)) 
-                   //|| (trailCircuit && this.route[0].start == this.route[this.route.length - 1].end && fullRoute.geometry.coordinates.length <= 5));    //lazy fix for circuits not forming due to start == end       
                    || (trailCircuit && this.route[0].start == this.route[this.route.length - 1].end && this.route.length > 1 && j == 0)); //solves case where full circuit will not build line because start == end
             if (trailCircuit && this.route[0].start == this.route[this.route.length - 1].end) {
                 dayRoute.geometry.coordinates.push(trailFeature.geometry.coordinates[0]);
                 fullRoute.geometry.coordinates.push(trailFeature.geometry.coordinates[0]);
             }
-            //        i = this.isPositiveDirection ? i + 1 : i - 1;
-            // if (i < 0) i = trailFeature.geometry.coordinates.length - 1;
-            // if (i >= trailFeature.geometry.coordinates.length) i = 0;
-            // dayRoute.geometry.coordinates.push(trailFeature.geometry.coordinates[i]);
-            // fullRoute.geometry.coordinates.push(trailFeature.geometry.coordinates[i]);    
             break;
         }
     }                    
@@ -80,8 +75,10 @@ function updateGeoJSON() {
                 layer.bindPopup('Night ' + campsiteIndex);
                 layer.setIcon(tentIcon);
             } else if (layer.feature.properties && layer.feature.properties.folderId == trailheadFolder.id && layer.feature.properties.title == this.route[0].start.properties.title) {
+                layer.bindPopup('Start');
                 layer.setIcon(startIcon);
             } else if (layer.feature.properties && layer.feature.properties.folderId == trailheadFolder.id && layer.feature.properties.title == this.route[this.route.length - 1].end.properties.title) {
+                layer.bindPopup('Finish');
                 layer.setIcon(endIcon);
             }
         } else if (layer.feature.geometry.type == "LineString") {
@@ -91,6 +88,10 @@ function updateGeoJSON() {
             if (layer.feature.properties.title.charAt(layer.feature.properties.title.length - 1) % 2 == 0) layer.setStyle({color :'#ff7d7d'});
         }
     });
+    console.log(exportedRoute);
+}
 
+function exportGeoJSON() {
+    exportedRoute.features.push(fullRoute);
     console.log(exportedRoute);
 }
